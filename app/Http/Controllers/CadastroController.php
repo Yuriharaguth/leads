626f52cdebdefce4d5344af7abbcf8700183ef79
Yuri <yuri@xbrain.com.br>
2018-05-09 15:51:15 -0300

    #0000 First commit

---
+++ app/Http/Controllers/CadastroController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class CadastroController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+}
