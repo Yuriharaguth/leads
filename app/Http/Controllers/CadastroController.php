3eaace6a718ab3a86e9bf96ba219d41096f6d617
Yuri <yuri@xbrain.com.br>
2018-05-10 17:38:43 -0300

    #0000 IMplementa chamda de servico

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
 
 class CadastroController extends Controller
 {
@@ -16,14 +17,32 @@ class CadastroController extends Controller
 
     public function store(Request $request)
     {
-        //dd($request->all());
 
+        //192.168.1.60:8086/api/captacao-leads/salvar
+
+        $client = new Client([
+            'base_uri' => 'http://localhost:8086/',
+        ]);
+
+        $request->merge([ 'idEmpresa' => 200 ]);
+
+        $r = $client->request('POST', 'http://localhost:8086/api/captacao-leads/salvar', ['body' => $request]);
+/*
+        $response = $client->post('api/captacao-leads/salvar', [
+            'debug' => TRUE,
+            'body' => $request->all(),
+            'headers' => [
+              'Content-Type' => 'application/x-www-form-urlencoded',
+            ]
+        ]);*/
+        dd($request-all());
+/*
         $this->validate($request,
         [
             'nome' => 'required|alpha',
             'telefone' => 'required|numeric'
         ]);
-
+*/
     }
 
 }
